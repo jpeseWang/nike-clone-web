8d1bfad9b33fcfc47fd4de762191cd88538a8a0a
vutruonghuy123 <vutruonghuy123@gmail.com>
2023-08-06 10:17:06 +0700

    fix some stuff

--- src/layouts/Header/Header.tsx
+++ src/layouts/Header/Header.tsx
@@ -16,11 +16,17 @@ export const Header = (): JSX.Element => {
                  <img src={dunking} alt="" className="header-navbar__dunking"/>  
               </div>
                 <ul className="header-navbar__list">
-                  <li className="header-navbar__list-item">Find a store</li>
-                  <li className="header-navbar__list-item">Help</li>
-                  <li className="header-navbar__list-item">Join us</li>
                   <li className="header-navbar__list-item">
-                    Sign in
+                    <a href="" className="header-navbar__list-item__linkk">Find a store</a>
+                  </li>
+                  <li className="header-navbar__list-item">
+                    <a href="" className="header-navbar__list-item__link">Help</a>
+                  </li>
+                  <li className="header-navbar__list-item">
+                    <a href="" className="header-navbar__list-item__link">Join us</a>
+                  </li>
+                  <li className="header-navbar__list-item">
+                    <a href="" className="header-navbar__list-item__link">Sign in</a>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="header-navbar__list-item__arrow-right">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                     </svg>
@@ -44,12 +50,17 @@ export const Header = (): JSX.Element => {
                 </li>
                 <li className="header-navbar__list-2__icon">
                   <input type="text" className="list-2__search-bar" placeholder="Search"/>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="list-2__icon list-2__icon__heart">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-                  </svg>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="list-2__icon list-2__icon__shopping-bag">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
-                  </svg>  
+                  <a href="" className="header-navbar__list-2__icon__link">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="list-2__icon list-2__icon__heart">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+                    </svg>
+                  </a>
+                  <a href="" className="header-navbar__list-2__icon__link">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="list-2__icon list-2__icon__shopping-bag">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>  
+                  </a>
+                  
                 </li>
                 <div className="featured__modal">
                   <div className="featured__modal--overlay"></div>
